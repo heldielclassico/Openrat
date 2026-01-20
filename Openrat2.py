6ab6708f2d09b4dbe093011f54cc29cf11b71f25
heldielclassico <113118436+heldielclassico@users.noreply.github.com>
2026-01-20 19:29:50 +0700

    Update Openrat2.py

--- Openrat2.py
+++ Openrat2.py
@@ -17,7 +17,7 @@ from pypdf import PdfReader
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_community.vectorstores import Chroma
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain.docstore.document import
+from langchain.docstore.document import Document
 
 # --- 1. KONFIGURASI HALAMAN ---
 st.set_page_config(page_title="Asisten POLTESA", page_icon="🎓")
